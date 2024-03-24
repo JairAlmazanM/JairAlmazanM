bb645db1d480a66c8af59715cba0291906a0a278
Jair Almazan <add.jair.almazan@gmail.com>
2024-03-24 11:10:45 -0600

    tailwind

---
+++ docs/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+// tailwind.config.js
+
+  module.exports = {
+    content: [
+      './_includes/**/*.html',
+      './_layouts/**/*.html',
+      './_posts/*.md',
+      './*.html',
+    ],
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [
+      require('@tailwindcss/typography'),
+    ],
+  }
